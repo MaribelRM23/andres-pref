6490a4e88f8d5d6e085a67dbc17538cceaee1a76
MaribelRM23 <mrestrepo7036@misena.edu.co>
2023-03-29 00:08:29 -0500

    andres

---
+++ estados-react/src/App.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import './App.css'
+import { Button } from 'react-bootstrap';
+import Tarjeta  from './components/Tarjeta'
+
+function App() {
+  return (
+   <Tarjeta 
+   
+
+   /> 
+
+    
+
+  )
+}
+
+export default App
